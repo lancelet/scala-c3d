8759f3b85414f513f088672e110b7251e23a97ca
Jonathan Merritt <j.s.merritt@gmail.com>
2013-03-22 19:11:52 +1100

    Reset repository - rethink of structure.

--- src/main/scala/c3d/processorType.scala
+++
@@ -1,6 +0,0 @@
-package c3d
-
-sealed trait ProcessorType
-object Intel extends ProcessorType
-object DEC extends ProcessorType
-object SGIMIPS extends ProcessorType
--- src/main/scala/c3d/storedDataType.scala
+++
@@ -1,5 +0,0 @@
-package c3d
-
-sealed trait StoredDataType
-object SDTInteger extends StoredDataType
-object SDTFloat extends StoredDataType
