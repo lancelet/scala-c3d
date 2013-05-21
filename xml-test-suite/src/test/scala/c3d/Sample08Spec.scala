53379842aade9ef3b70116fefaa4002fd2fc4ec6
Jonathan Merritt <j.s.merritt@gmail.com>
2013-05-21 11:14:48 +1000

    Added extra Sample08 tests. These currently fail because the offset to the video/analog data section differs.  Need to implement an XML diff which ignores these things, or create multiple reference XML files.

--- xml-test-suite/src/test/scala/c3d/Sample08Spec.scala
+++ xml-test-suite/src/test/scala/c3d/Sample08Spec.scala
@@ -9,20 +9,37 @@ class Sample08Spec extends FeatureSpec {
 
   feature("Sample08 files should be read correctly") {
 
+    lazy val refXML: String = readFileToString(fetchXMLFile("sample08/sample08.xml"))
+
     scenario("EB015PI.c3d") {
       val readXML: String = c3d2xmlString(fetchC3DFile("sample08/EB015PI.c3d")).getOrElse(fail())
-      val refXML: String = readFileToString(fetchXMLFile("sample08/sample08.xml"))
       val diff: Diff = new Diff(refXML, readXML)
       assert(diff.similar())
     }
 
-    scenario("TESTAPI.c3d") (pending)
+    scenario("TESTAPI.c3d") {
+      val readXML: String = c3d2xmlString(fetchC3DFile("sample08/TESTAPI.c3d")).getOrElse(fail())
+      val diff: Diff = new Diff(refXML, readXML)
+      assert(diff.similar())
+    }
 
-    scenario("TESTBPI.c3d") (pending)
+    scenario("TESTBPI.c3d") {
+      val readXML: String = c3d2xmlString(fetchC3DFile("sample08/TESTBPI.c3d")).getOrElse(fail())
+      val diff: Diff = new Diff(refXML, readXML)
+      assert(diff.similar())
+    }
 
-    scenario("TESTCPI.c3d") (pending)
+    scenario("TESTCPI.c3d") {
+      val readXML: String = c3d2xmlString(fetchC3DFile("sample08/TESTCPI.c3d")).getOrElse(fail())
+      val diff: Diff = new Diff(refXML, readXML)
+      assert(diff.similar())
+    }
 
-    scenario("TESTDPI.c3d") (pending)
+    scenario("TESTDPI.c3d") {
+      val readXML: String = c3d2xmlString(fetchC3DFile("sample08/TESTDPI.c3d")).getOrElse(fail())
+      val diff: Diff = new Diff(refXML, readXML)
+      assert(diff.similar())
+    }
 
   }
 
