26e14a4f9129d42747f72f13b40fb3101b6af394
Jonathan Merritt <j.s.merritt@gmail.com>
2013-05-21 21:11:43 +1000

    Added DEC and SGIMIPS BinaryFormats (floating-point not implemented yet).

--- c3d/src/main/scala/c3d/io/BinaryFormat.scala
+++ c3d/src/main/scala/c3d/io/BinaryFormat.scala
@@ -6,19 +6,19 @@ import c3d.ProcessorType
 /** Binary representation of `Float` and `Byte`. */
 private [io] trait BinaryFormat {
 
-  /** Converts `Byte`s to a `Float`.
+  /** Converts 4 `Byte`s to a `Float`.
     * 
     * The bytes are supplied in the order they appear in the file.
     */
   def bytesToFloat(b0: Byte, b1: Byte, b2: Byte, b3: Byte): Float
 
-  /** Converts `Byte`s to an `Int`.
+  /** Converts 2 `Byte`s to an `Int`.
     * 
     * The bytes are supplied in the order they appear in the file.
     */
   def bytesToInt(b0: Byte, b1: Byte): Int
 
-  /** Converts `Byte`s to an unsigned `Int`.
+  /** Converts 2 `Byte`s to an unsigned `Int`.
     *
     * The bytes are supplied in the order they appear in the file.
     */
@@ -28,21 +28,45 @@ private [io] trait BinaryFormat {
 
 private [io] object BinaryFormat {
 
-  /** Represents the binary format used by Intel processors. */
+  /** Little-endian conversion of 2 bytes to `Int`. */
+  private def bytesToIntLittle(b0: Byte, b1: Byte): Int = (b0.toInt) + (b1.toInt << 8)
+
+  /** Big-endian conversion of 2 bytes to `Int`. */
+  private def bytesToUIntLittle(b0: Byte, b1: Byte): Int = (b0 & 0xFF) + ((b1 & 0xFF) << 8)
+
+  /** BinaryFormat used by Intel processors. */
   object Intel extends BinaryFormat {
     def bytesToFloat(b0: Byte, b1: Byte, b2: Byte, b3: Byte): Float = {
       val i = (b0 & 0xFF) + ((b1 & 0xFF) << 8) + ((b2 & 0xFF) << 16) + ((b3 & 0xFF) << 24)
       java.lang.Float.intBitsToFloat(i)
     }
-    def bytesToInt(b0: Byte, b1: Byte): Int = (b0.toInt) + (b1.toInt << 8)
-    def bytesToUInt(b0: Byte, b1: Byte): Int = (b0 & 0xFF) + ((b1 & 0xFF) << 8)
+    def bytesToInt(b0: Byte, b1: Byte): Int = bytesToIntLittle(b0, b1)
+    def bytesToUInt(b0: Byte, b1: Byte): Int = bytesToUIntLittle(b0, b1)
+  }
+
+  /** BinaryFormat used by DEC processors. */
+  object DEC extends BinaryFormat {
+    def bytesToFloat(b0: Byte, b1: Byte, b2: Byte, b3: Byte): Float = {
+      throw new NotImplementedError("TODO: NOT YET IMPLEMENTED") // TODO
+    }
+    def bytesToInt(b0: Byte, b1: Byte): Int = bytesToIntLittle(b0, b1)
+    def bytesToUInt(b0: Byte, b1: Byte): Int = bytesToUIntLittle(b0, b1)
+  }
+
+  /** BinaryFormat used by SGIMIPS processors. */
+  object SGIMIPS extends BinaryFormat {
+    def bytesToFloat(b0: Byte, b1: Byte, b2: Byte, b3: Byte): Float = {
+      throw new NotImplementedError("TODO: NOT YET IMPLEMENTED") // TODO
+    }
+    def bytesToInt(b0: Byte, b1: Byte): Int = bytesToIntLittle(b1, b0)    // swap b0, b1
+    def bytesToUInt(b0: Byte, b1: Byte): Int = bytesToUIntLittle(b1, b0)  // swap b0, b1
   }
 
   /** Returns a [[BinaryFormat]] from a [[ProcessorType]]. */
   private [io] def fromProcessorType(p: ProcessorType): BinaryFormat = p match {
     case ProcessorType.Intel   => Intel
-    case ProcessorType.DEC     => Intel   // TODO: Implement this BinaryFormat
-    case ProcessorType.SGIMIPS => Intel   // TODO: Implement this BinaryFormat
+    case ProcessorType.DEC     => DEC
+    case ProcessorType.SGIMIPS => SGIMIPS
   }
 
 }
--- c3d/src/test/scala/c3d/io/BinaryFormatSpec.scala
+++ c3d/src/test/scala/c3d/io/BinaryFormatSpec.scala
@@ -15,14 +15,54 @@ class BinaryFormatSpec extends FunSpec {
 
     it("should read Intel signed integers (positive and negative)") {
       val bf = BinaryFormat.fromProcessorType(ProcessorType.Intel)
+      // little-endian
       assert(bf.bytesToInt(b(0x0C), b(0xFF)) === -244)
       assert(bf.bytesToInt(b(0x36), b(0x07)) === 1846)
     }
 
     it("should read Intel unsigned integers") {
       val bf = BinaryFormat.fromProcessorType(ProcessorType.Intel)
+      // little-endian
       assert(bf.bytesToUInt(b(0x0C), b(0xFF)) === 65292)
       assert(bf.bytesToUInt(b(0x36), b(0x07)) === 1846)
+    }
+
+    it("should read DEC floating point numbers") {
+      val bf = BinaryFormat.fromProcessorType(ProcessorType.DEC)
+      assert(bf.bytesToFloat(b(0xAA), b(0x3E), b(0xAB), b(0xAA)) === 0.0833333f)
+    }
+
+    it("should read DEC signed integers (positive and negative)") {
+      val bf = BinaryFormat.fromProcessorType(ProcessorType.DEC)
+      // little-endian
+      assert(bf.bytesToInt(b(0x0C), b(0xFF)) === -244)
+      assert(bf.bytesToInt(b(0x36), b(0x07)) === 1846)
+    }
+
+    it("should read DEC unsigned integers") {
+      val bf = BinaryFormat.fromProcessorType(ProcessorType.DEC)
+      // little-endian
+      assert(bf.bytesToUInt(b(0x0C), b(0xFF)) === 65292)
+      assert(bf.bytesToUInt(b(0x36), b(0x07)) === 1846)
+    }
+
+    it("should read SGI floating point numbers") {
+      val bf = BinaryFormat.fromProcessorType(ProcessorType.SGIMIPS)
+      assert(bf.bytesToFloat(b(0x3D), b(0xAA), b(0xAA), b(0xAB)) === 0.0833333f)
+    }
+
+    it("should read SGI signed integers (positive and negative)") {
+      val bf = BinaryFormat.fromProcessorType(ProcessorType.SGIMIPS)
+      // big-endian
+      assert(bf.bytesToInt(b(0xFF), b(0x0C)) === -244)
+      assert(bf.bytesToInt(b(0x07), b(0x36)) === 1846)
+    }
+
+    it("should read SGI unsigned integers") {
+      val bf = BinaryFormat.fromProcessorType(ProcessorType.SGIMIPS)
+      // big-endian
+      assert(bf.bytesToUInt(b(0xFF), b(0x0C)) === 65292)
+      assert(bf.bytesToUInt(b(0x07), b(0x36)) === 1846)
     }
 
   }
